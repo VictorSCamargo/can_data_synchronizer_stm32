bd8e79579f48fde0d8a24659350ed4a6634dd4f9
VictorSCamargo <victor.s.camargo@gmail.com>
2022-10-04 14:58:00 -0300

    Upgrade
    Better description and adjustments

--- can_mvs/Inc/can_mvs_config.h
+++ can_mvs/Inc/can_mvs_config.h
@@ -11,35 +11,56 @@
 #ifdef __cplusplus
  extern "C" {
 #endif
-
-/* include the HAL for your board model */
-#include "stm32f4xx_hal.h"
 	 
+/*----------------------------------------------*/
+	 
+/* include the HAL for your board model */
+#include "stm32f1xx_hal.h"
+
 /* to debug with only one STM32 board */
-#define CAN_MVS_LOOPBACK_MODE
+//#define CAN_MVS_LOOPBACK_MODE
+	 
+/* CUSTOM USER CODE BEGIN */
+
+#define CUSTOM_SENDER_ID (0x123)
+#define CUSTOM_SENDER_ID (0x456)
+	 
+/* CUSTOM USER CODE END */
 
 /* this should be the ID that the other device's CAN filter was configured to receive*/
-#define CAN_MVS_SENDER_ID 0x407
+#define CAN_MVS_SENDER_ID CUSTOM_SENDER_ID
 
 /* the device will receive messages that have this ID */
-#define CAN_MVS_FILTER_ID 0x103
+#define CAN_MVS_FILTER_ID CUSTOM_FILTER_ID
 
 #ifdef CAN_MVS_LOOPBACK_MODE
-	#define HAL_CAN_FILTER_ID CAN_MVS_SENDER_ID //is the same in the loopback mode
+	#define HAL_CAN_FILTER_ID (CAN_MVS_SENDER_ID<<5) //should be the same in the loopback mode
 #else
-	#define HAL_CAN_FILTER_ID CAN_MVS_FILTER_ID
+	#define HAL_CAN_FILTER_ID (CAN_MVS_FILTER_ID<<5)
 #endif
+
 
 /**
   * Add the IDs for your structs in this enum.
   * - first ID should be equal to zero.
-  * - do not remove ID_STRUCT_MAX.
+  * - do not remove ID_DATA_MAX / ID_FNC_MAX.
   */
+
+/* Up to 32 IDs + ID_DATA_MAX */
 typedef enum
 {
-	ID_STRUCT_EXAMPLE = 0,
-	ID_STRUCT_MAX
-}CAN_MVS_struct_id;
+	ID_DATA_EXAMPLE = 0,
+	ID_DATA_MAX
+} CAN_MVS_data_id;
+
+/* Up to 32 IDs + ID_FNC_MAX */
+typedef enum
+{
+	ID_FNC_EXAMPLE = 0,
+	ID_FNC_MAX
+} CAN_MVS_functions_id;
+
+/*----------------------------------------------*/
 
 #ifdef __cplusplus
 }
--- can_mvs/Src/can_mvs.cpp
+++ can_mvs/Src/can_mvs.cpp
@@ -21,8 +21,8 @@ CanMvs::CanMvs(void)
 	count_sent_bytes = 0;
 	max_sendable_bytes = 0;
 
-	id_shipping_struct = ID_STRUCT_MAX;
-	id_receipt_struct = ID_STRUCT_MAX;
+	id_shipping_struct = ID_DATA_MAX;
+	id_receipt_struct = ID_DATA_MAX;
 
 	sent_header = false;
 
@@ -33,7 +33,7 @@ CanMvs::CanMvs(void)
 
 	ptr_hcan = NULL;
 
-	for(int i = 0; i < ID_STRUCT_MAX; i++)
+	for(int i = 0; i < ID_DATA_MAX; i++)
 	{
 		ptr_structs[i] = NULL;
 	}
@@ -57,60 +57,93 @@ CanMvs::CanMvs(void)
 void CanMvs::init(CAN_HandleTypeDef *hcan)
 {
 	ptr_hcan = hcan;
+	
+	HAL_CAN_Start(ptr_hcan);
+
+  HAL_CAN_ActivateNotification(ptr_hcan, CAN_IT_RX_FIFO1_MSG_PENDING);
 }
 
-bool CanMvs::is_struct_id_valid(CAN_MVS_struct_id struct_id)
+bool CanMvs::is_struct_id_valid(CAN_MVS_data_id struct_id)
 {
 	int id_value = struct_id;
 
-	if((id_value < 0) || (id_value >= (uint8_t)ID_STRUCT_MAX))
+	if((id_value < 0) || (id_value >= (uint8_t)ID_DATA_MAX))
 	{
 		return false;
 	}
 	return true;
 }
 
-CAN_MVS_status CanMvs::get_app_status(void)
-{
-	if(!was_all_pending_data_sent())
-	{
-		return CAN_MVS_BUSY;
-	}
-	return CAN_MVS_OK;
-}
-
-CAN_MVS_status CanMvs::connect_struct_to_id(
-	CAN_MVS_struct_id struct_id,
+CAN_status CanMvs::connect_struct_to_id(
+	CAN_MVS_data_id struct_id,
 	uint8_t *ptr_to_struct,
 	uint8_t size_of_struct
 )
 {
 	if(!is_struct_id_valid(struct_id))
 	{
-		return CAN_MVS_INVALID_ID;
+		return CAN_STATUS_INVALID_ID;
 	}
 	ptr_structs[struct_id] = ptr_to_struct;
 	size_of_structs[struct_id] = size_of_struct;
-	return CAN_MVS_OK;
+	return CAN_STATUS_OK;
 }
 
-CAN_MVS_status CanMvs::shipping_application(void)
+CAN_status CanMvs::shipping_application(void)
 {
-	CAN_MVS_status status = CAN_MVS_OK;
+	CAN_status status = CAN_STATUS_OK;
 
-	if(!was_all_pending_data_sent())
+	if(is_there_any_data_id_flag_marked(CAN_MARK_TO_BE_REQUESTED))
 	{
-		if(!is_HAL_CAN_busy())
+		if(is_HAL_CAN_busy())
 		{
-			if(!sent_header)
+			return CAN_STATUS_BUSY;
+		}
+		prepare_package_to_be_send(CAN_DATA_TYPE_ASK_FOR_DATA);
+		status = send_package();
+
+		if(status == CAN_STATUS_OK)
+		{
+			clear_pending_data_flags(CAN_MARK_TO_BE_REQUESTED);
+		}
+	}
+
+	if(is_there_any_func_id_flag_marked(CAN_MARK_TO_BE_REQUESTED))
+	{
+		if(is_HAL_CAN_busy())
+		{
+			return CAN_STATUS_BUSY;
+		}
+		prepare_package_to_be_send(CAN_DATA_TYPE_HAVE_A_FUNCTION_RUN);
+		status = send_package();
+
+		if(status == CAN_STATUS_OK)
+		{
+			clear_pending_func_flags(CAN_MARK_TO_BE_REQUESTED);
+		}
+	}
+
+	if(!were_all_pending_data_packages_sent())
+	{
+		if(is_HAL_CAN_busy())
+		{
+			return CAN_STATUS_BUSY;
+		}
+
+		if(!sent_header)
+		{
+			prepare_package_to_be_send(CAN_DATA_TYPE_HEADER);
+			status = send_package();
+
+			if(status == CAN_STATUS_OK)
 			{
-				send_header();
+				sent_header = true;
 			}
-			else
-			{
-				prepare_package();
-				status = send_package();
-			}
+		}
+		else
+		{
+			prepare_package_to_be_send(CAN_DATA_TYPE_STANDARD);
+			status = send_package();
 		}
 	}
 	return status;
@@ -129,11 +162,11 @@ void CanMvs::receive_data_callback(void)
 	HAL_CAN_ActivateNotification(ptr_hcan, CAN_IT_RX_FIFO1_MSG_PENDING);
 }
 
-CAN_MVS_status CanMvs::start_shipping(CAN_MVS_struct_id id)
+CAN_status CanMvs::start_shipping_data(CAN_MVS_data_id id)
 {
-	if(!was_all_pending_data_sent() || am_i_receiving_this_id(id))
+	if(!were_all_pending_data_packages_sent() || am_i_receiving_this_id(id))
 	{
-		return CAN_MVS_BUSY;
+		return CAN_STATUS_BUSY;
 	}
 
 	uint16_t struct_size;
@@ -143,7 +176,7 @@ CAN_MVS_status CanMvs::start_shipping(CAN_MVS_struct_id id)
 
 	if(ptr_structs[id] == NULL)
 	{
-		return CAN_MVS_NULL_POINTER;
+		return CAN_STATUS_NULL_POINTER;
 	}
 
 	struct_size = size_of_structs[id];
@@ -160,7 +193,7 @@ CAN_MVS_status CanMvs::start_shipping(CAN_MVS_struct_id id)
 	
 	if(ptr_struct_to_send == NULL)
 	{
-		return CAN_MVS_ERROR;
+		return CAN_STATUS_ERROR;
 	}
 
 	memcpy(ptr_struct_to_send, ptr_choosen_struct, struct_size);
@@ -173,41 +206,102 @@ CAN_MVS_status CanMvs::start_shipping(CAN_MVS_struct_id id)
 	id_shipping_struct = id;
 	sent_header = false;
 
-	return CAN_MVS_OK;
+	return CAN_STATUS_OK;
+}
+
+bool CanMvs::is_there_any_data_id_flag_marked(CAN_mark_type source)
+{
+	if(flags_data_requisition[source] != 0)
+	{
+		return true;
+	}		
+	return false;
+}
+
+bool CanMvs::is_data_id_flag_marked(CAN_mark_type source, CAN_MVS_data_id id)
+{
+	if(flags_data_requisition[source] & 1<<(uint8_t)id)
+	{
+		return true;
+	}
+	return false;
+}
+
+void CanMvs::mark_data_id_flag(CAN_mark_type source, CAN_MVS_data_id id)
+{
+	flags_data_requisition[source] |= 1<<id;
+}
+
+void CanMvs::unmark_data_id_flag(CAN_mark_type source, CAN_MVS_data_id id)
+{
+	flags_data_requisition[source] &= ~(1<<id);
+}
+
+bool CanMvs::is_there_any_func_id_flag_marked(CAN_mark_type source)
+{
+	if(flags_func_requisition[source] != 0)
+	{
+		return true;
+	}		
+	return false;
+}
+bool CanMvs::is_func_id_flag_marked(CAN_mark_type source, CAN_MVS_functions_id id)
+{
+	if(flags_func_requisition[source] & 1<<(uint8_t)id)
+	{
+		return true;
+	}
+	return false;
+}
+
+void CanMvs::mark_func_id_flag(CAN_mark_type source, CAN_MVS_functions_id id)
+{
+	flags_func_requisition[source] |= 1<<id;
+}
+
+void CanMvs::unmark_func_id_flag(CAN_mark_type source, CAN_MVS_functions_id id)
+{
+	flags_func_requisition[source] &= ~(1<<id);
+}
+
+void CanMvs::clear_pending_data_flags(CAN_mark_type source)
+{
+	flags_data_requisition[source] = 0;
+}
+
+void CanMvs::clear_pending_func_flags(CAN_mark_type source)
+{
+	flags_func_requisition[source] = 0;
 }
 
 /* End of public methods ------------------------------------ */
 
-bool CanMvs::am_i_receiving_this_id(CAN_MVS_struct_id id)
+bool CanMvs::am_i_receiving_this_id(CAN_MVS_data_id id)
 {
-	if((id == id_receipt_struct) && !was_all_pending_data_received())
+	if((id == id_receipt_struct) && !were_all_pending_data_packages_received())
 	{
 		return true;
 	}
 	return false;
 }
 
-bool CanMvs::is_rxdata_header(void)
+CAN_data_type CanMvs::identify_received_data_type(void)
 {
-	if(RxData[0] == ID_DATA_HEADER)
-	{
-		return true;
-	}
-	return false;
+	return (CAN_data_type)RxData[0];
 }
 
-CAN_MVS_struct_id CanMvs::identify_received_struct_id(void)
+CAN_MVS_data_id CanMvs::identify_received_struct_id(void)
 {
-	return (CAN_MVS_struct_id)RxData[1];
+	return (CAN_MVS_data_id)RxData[1];
 }
 
-CAN_MVS_status CanMvs::send_package(void)
+CAN_status CanMvs::send_package(void)
 {
 	if(HAL_CAN_AddTxMessage(ptr_hcan, &TxHeader, TxData, &TxMailbox) != HAL_OK)
 	{
-		return CAN_MVS_ERROR;
+		return CAN_STATUS_ERROR;
 	}
-	return CAN_MVS_OK;
+	return CAN_STATUS_OK;
 }
 
 bool CanMvs::is_HAL_CAN_busy(void)
@@ -224,46 +318,60 @@ void CanMvs::set_package_data_type(CAN_data_type type)
 	TxData[0] = (uint8_t)type;
 }
 
-void CanMvs::set_package_struct_id(CAN_MVS_struct_id id)
+void CanMvs::set_package_struct_id(CAN_MVS_data_id id)
 {
 	TxData[1] = (uint8_t)id;
 }
 
-CAN_MVS_status CanMvs::send_header(void)
-{	
-	set_package_data_type(ID_DATA_HEADER);
-	set_package_struct_id(id_shipping_struct);
-	
-	if(send_package() != CAN_MVS_OK)
-	{
-		return CAN_MVS_ERROR;
-	}
-
-	set_package_data_type(ID_DATA_STANDARD);
-				
-	sent_header = true;
-
-	return CAN_MVS_OK;
-}
-
-void CanMvs::prepare_package(void)
+void CanMvs::prepare_package_to_be_send(CAN_data_type data_type)
 {
-	/* first byte is reserved*/
-	for(int i = 1; i < 8; i++)
+	if(data_type == CAN_DATA_TYPE_STANDARD)
 	{
-		TxData[i] = *ptr_struct_to_send_bytes;
-		count_sent_bytes++;
-		ptr_struct_to_send_bytes++;
+		set_package_data_type(CAN_DATA_TYPE_STANDARD);
 
-		if(was_all_pending_data_sent())
+		/* first byte is reserved*/
+		for(int i = 1; i < 8; i++)
 		{
-			free(ptr_struct_to_send); //do not remove
-			break;
+			TxData[i] = *ptr_struct_to_send_bytes;
+			count_sent_bytes++;
+			ptr_struct_to_send_bytes++;
+
+			if(were_all_pending_data_packages_sent())
+			{
+				free(ptr_struct_to_send); //do not remove
+				break;
+			}
 		}
 	}
+
+	else if(data_type == CAN_DATA_TYPE_HEADER)
+	{
+		set_package_data_type(CAN_DATA_TYPE_HEADER);
+		set_package_struct_id(id_shipping_struct);
+	}
+
+	else if(data_type == CAN_DATA_TYPE_ASK_FOR_DATA)
+	{	
+		uint8_t *conversor_ptr = (uint8_t*) &flags_data_requisition[CAN_MARK_TO_BE_REQUESTED];
+		set_package_data_type(CAN_DATA_TYPE_ASK_FOR_DATA);
+		TxData[1] = conversor_ptr[3]; //Little-endian is the default memory format for ARM processors.
+		TxData[2] = conversor_ptr[2];
+		TxData[3] = conversor_ptr[1];
+		TxData[4] = conversor_ptr[0];
+	}
+
+	else if(data_type == CAN_DATA_TYPE_HAVE_A_FUNCTION_RUN)
+	{
+		uint8_t *conversor_ptr = (uint8_t*) &flags_func_requisition[CAN_MARK_TO_BE_REQUESTED];
+		set_package_data_type(CAN_DATA_TYPE_HAVE_A_FUNCTION_RUN);
+		TxData[1] = conversor_ptr[3];
+		TxData[2] = conversor_ptr[2];
+		TxData[3] = conversor_ptr[1];
+		TxData[4] = conversor_ptr[0];
+	}
 }
 
-bool CanMvs::was_all_pending_data_sent(void)
+bool CanMvs::were_all_pending_data_packages_sent(void)
 {
 	if(count_sent_bytes != max_sendable_bytes)
 	{
@@ -272,7 +380,7 @@ bool CanMvs::was_all_pending_data_sent(void)
 	return true;
 }
 
-bool CanMvs::was_all_pending_data_received(void)
+bool CanMvs::were_all_pending_data_packages_received(void)
 {
 	if(count_received_bytes != max_receivable_bytes)
 	{
@@ -283,20 +391,48 @@ bool CanMvs::was_all_pending_data_received(void)
 
 void CanMvs::process_received_package(void)
 {
-	if(is_rxdata_header())
+	switch(identify_received_data_type())
 	{
-		start_receiving(identify_received_struct_id());
-	}
-	else
-	{
-		if(!was_all_pending_data_received())
-		{
-			unload_received_package();
-		}
+		case CAN_DATA_TYPE_STANDARD:
+			if(!were_all_pending_data_packages_received())
+			{
+				unload_received_package();
+			}
+			break;
+
+		case CAN_DATA_TYPE_HEADER:
+			start_receiving(identify_received_struct_id());
+			break;
+		
+		case CAN_DATA_TYPE_ASK_FOR_DATA:
+			callback_set_asked_flags(CAN_DATA_TYPE_ASK_FOR_DATA);
+			break;
+		
+		case CAN_DATA_TYPE_HAVE_A_FUNCTION_RUN:
+			callback_set_asked_flags(CAN_DATA_TYPE_HAVE_A_FUNCTION_RUN);
+			break;
+		default:
+			break;
 	}
 }
 
-CAN_MVS_status CanMvs::start_receiving(CAN_MVS_struct_id id)
+void CanMvs::callback_set_asked_flags(CAN_data_type received_data_type)
+{
+	uint32_t united_data = (
+		RxData[4] | (RxData[3] << 8) | (RxData[2] << 16) | (RxData[1] << 24)
+	);
+
+	if(received_data_type == CAN_DATA_TYPE_ASK_FOR_DATA)
+	{
+		flags_data_requisition[CAN_MARK_TO_BE_PROCESSED] |= united_data; //sets 1 to those that arent
+	}
+	else
+	{
+		flags_func_requisition[CAN_MARK_TO_BE_PROCESSED] |= united_data;
+	}
+}
+
+CAN_status CanMvs::start_receiving(CAN_MVS_data_id id)
 {
 	uint16_t struct_size;
 	uint8_t* ptr_choosen_struct;
@@ -317,7 +453,7 @@ CAN_MVS_status CanMvs::start_receiving(CAN_MVS_struct_id id)
 	
 	if(ptr_struct_to_receive == NULL)
 	{
-		return CAN_MVS_ERROR;
+		return CAN_STATUS_ERROR;
 	}
 
 	memcpy(ptr_struct_to_receive, ptr_choosen_struct, struct_size);
@@ -328,7 +464,7 @@ CAN_MVS_status CanMvs::start_receiving(CAN_MVS_struct_id id)
 	max_receivable_bytes = struct_size;
 	count_received_bytes = 0;
 
-	return CAN_MVS_OK;
+	return CAN_STATUS_OK;
 }
 
 void CanMvs::copy_built_struct_to_destiny(void)
@@ -361,7 +497,7 @@ void CanMvs::unload_received_package(void)
 		count_received_bytes++;
 		ptr_struct_to_receive_bytes++;
 
-		if(was_all_pending_data_received())
+		if(were_all_pending_data_packages_received())
 		{
 			copy_built_struct_to_destiny();
 			free(ptr_struct_to_receive); //do not remove
